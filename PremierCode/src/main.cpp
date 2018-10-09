4a32fffba15f78bd984ae5aedfc05f9892d1f63c
NAme <amyf2502@usherbrooke.ca>
2018-10-08 20:48:20 -0400

    Code tel que jeudi 4 oct

--- PremierCode/src/main.cpp
+++ PremierCode/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <LibRobus.h>
-//int x = 0;
 
 
 void avancer(float distance,float vitesse = 0.5)
@@ -8,15 +7,15 @@ void avancer(float distance,float vitesse = 0.5)
     ENCODER_Reset(0);
     ENCODER_Reset(1);
     // limite de 2 m
-    distance = 3200 *distance/0.24;// Encodeur tour//Diametre roue en m
+    distance = 3200 *distance/0.25;// Encodeur tour//Diametre roue en m
     vitesse *= 0.95;
     float vitesseCorriger = vitesse;
     float correction;
     int x = 0;
     int g = 0,dr =0 ;
 
-    float p = 0.00001;
-    float i = 100;
+    float p = 0.00001;//0.000015;
+    float i = 800000;
     float d = 5;
 
     float erreur = 0;
@@ -36,7 +35,8 @@ void avancer(float distance,float vitesse = 0.5)
         erreur = g-dr;
         correction = 0;
         correction += erreur*p;
-        //correction += sommeErreur / i;
+        correction += sommeErreur / i;
+        //Serial.println(correction);
         //correction += (erreur - erreurPrec) / d;
         //correction /= 1000;
         vitesseCorriger += correction;
@@ -44,30 +44,28 @@ void avancer(float distance,float vitesse = 0.5)
         MOTOR_SetSpeed(1,vitesseCorriger);
         x = g;
         
-        //Serial.println(vitesseCorriger);
-        //sommeErreur += erreur;
+        Serial.println(vitesseCorriger);
+        sommeErreur += erreur;
         //erreurPrec = erreur;
         
     }Serial.println(x);
         Serial.println(dr);
 }
-void tourner(float angle,float vitesse,float rayon)
+void tourner(float angle,float vitesse,float rayon =0)
 {
     ENCODER_Reset(0);
     ENCODER_Reset(1);
-    vitesse *= .90; 
-    float vitesseCorriger = vitesse;
     float arcInt = PI * rayon / 180 * angle;
-    float arcExt = PI * (rayon + 0.1865) / 180 * angle;//distance entre roues (m)
+    float arcExt = PI * (rayon + 0.192) / 180 * angle;//distance entre roues (m)
 
     int inter = 1, ext = 0;
 
-    float distInt = abs(3200 *arcInt/0.22);// Encodeur tour//Diametre roue en m
-    float distExt = abs(3200 *arcExt/0.237);// Encodeur tour//Diametre roue en m
+    float distInt = abs(3200 *arcInt/0.25);// Encodeur tour//Diametre roue en m
+    float distExt = abs(3200 *arcExt/0.25);// Encodeur tour//Diametre roue en m
     float parcouruExt;
     float parcouruInt;
     
-    float p = 10;
+    float p = 8.5;
     float i = 0;
     float d = 0;
 
@@ -84,31 +82,37 @@ void tourner(float angle,float vitesse,float rayon)
         inter = 0;
         ext = 1;
     }
-    vitesseCorriger *= ratio;
+    vitesse *= .90; 
+    float vitesseCorriger = vitesse;
+    vitesse = vitesseCorriger/ratio;
+    Serial.println(ratio);
     MOTOR_SetSpeed(inter,vitesse);
     MOTOR_SetSpeed(ext, vitesseCorriger);
     
     while (distExt > parcouruExt)
     {
-        delay(100);
+        delay(50);
         parcouruExt = ENCODER_Read(ext);
         parcouruInt = ENCODER_Read(inter);
-        ratioReel = parcouruExt/parcouruInt;
-
-        if(parcouruInt>distInt)
+        if(p != 0)
+        {
+            ratioReel = parcouruExt/parcouruInt;
+            erreur = (ratio - ratioReel);
+        }
+        if(parcouruInt>=distInt)
         {
             MOTOR_SetSpeed(inter,0);
         }
 
-        erreur = (ratio - ratioReel);
+        
         correction = 0;
-        correction += erreur / p;
-        correction += sommeErreur *i;
-        correction += (erreur - erreurPrec) * d;
+        //correction += erreur / p;
+        //correction += sommeErreur *i;
+        //correction += (erreur - erreurPrec) * d;
 
         vitesseCorriger = vitesseCorriger + correction;
 
-        Serial.println(erreur);
+        Serial.println(ratioReel);
         Serial.println(correction);
         MOTOR_SetSpeed(ext,vitesseCorriger);
 
@@ -121,15 +125,15 @@ float acceleration(float speedFinal)
     ENCODER_Reset(0);
     ENCODER_Reset(1);
 
-    float g,dr,correction,erreur,sommeErreur,p = 10,i=1000000,d=1000,vitesseCorriger,erreurPrec = 0;
+    float g,dr,correction,erreur,sommeErreur,p = 1/1,i=1000000,d=1000,vitesseCorriger,erreurPrec = 0;
     float speed = 0.1;
     MOTOR_SetSpeed(0,speed);
     MOTOR_SetSpeed(1,speed);
-    delay(300);
+    
     while (speed < speedFinal)
     {
-        delay(20);
-        speed += .003;
+        delay(100);
+        speed += .015;
 
         g = ENCODER_Read(0);
         dr = ENCODER_Read(1);
@@ -143,12 +147,15 @@ float acceleration(float speedFinal)
         vitesseCorriger += correction;
         MOTOR_SetSpeed(0,speed);
         MOTOR_SetSpeed(1,speed+correction);
-        Serial.println(correction);
+        //Serial.println(correction);
         sommeErreur += erreur;
         erreurPrec = erreur;
     }
     MOTOR_SetSpeed(0,speedFinal);
-    float distanceFinale = ENCODER_Read(1)*3200/.24;
+    float distanceFinale = ENCODER_Read(1)/3200*.25;
+    Serial.println(distanceFinale);
+    Serial.print(g);
+    Serial.println(dr);
     return distanceFinale;
 }
 void stop()
@@ -160,18 +167,67 @@ void stop()
 void setup() {
     // put your setup code here, to run once:
     BoardInit();
+    while(!ROBUS_IsBumper(3))
+    {
+
+    }
 }
 void loop() {
     //acceleration(.5);
-    avancer(2,.5);
+    //avancer(2,.4);
     stop();
-    //tourner(-180,.2,.1);
+    while(!ROBUS_IsBumper(3))
+    {
+
+    }
+    
+    //tourner(90,.6,.129);
     stop();
     delay(1000);
     //stop();
-    //delay(1000);
-    /*Serial.println(ENCODER_ReadReset(0));
-    Serial.println(ENCODER_ReadReset(1));*/
-   // x++;
+    //tourner(45,.6,.129);
+    // Parcourt
+
+    float speed = 0.6f;
+    float rayon = 0.129;
+    avancer(1.8-acceleration(speed),speed);
+    tourner(-86,speed,rayon);
+    avancer(0.07,speed);
+    tourner(190,speed,rayon);
+    avancer(0.05,speed);
+    tourner(-90,speed,rayon);
+    avancer(0.01,speed);
+    tourner(45,speed,rayon);
+    avancer(0.15,speed);
+    tourner(-90,speed,rayon);
+    avancer(.40,speed);
+    tourner(35,speed,rayon);
+    avancer(0.32,speed);
+    tourner(13.5,speed,rayon);
+    avancer(.78,speed);
+
+    // U turn
+    tourner(45,speed,rayon);
+    stop();
+    tourner(-290,speed,0);
+    stop();
+    tourner(45,speed,rayon);
+
+    // Retour
+    avancer(0.78,speed);
+    tourner(-5,speed,rayon);
+    avancer(0.30,speed);
+    tourner(-25,speed,rayon);
+    avancer(.50,speed);
+    tourner(90,speed,rayon);
+    avancer(0.18,speed);
+    tourner(-45,speed,rayon);
+    stop();
+    tourner(80,speed,rayon);
+    avancer(0.05,speed);
+    tourner(-180,speed,rayon);
+    avancer(0.05,speed);
+    tourner(90,speed,rayon);
+    avancer(2,speed); 
 }
 
